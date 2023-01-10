14ee8e65c190e8640f42d8341b437aac4e11794e
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-09 22:16:44 -0300

    Usar nginx como proxy inverso

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 
-uwsgi --socket :8000 --master --enable-threads --module config.wsgi
+uwsgi --socket :8000 --master --enable-threads --module web.wsgi
