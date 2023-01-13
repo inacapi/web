61038785a4cb7f18434ca98b7a35c8393f2ed4d5
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-13 18:02:10 -0300

    Permitir usar admin sin cuenta de la aplicación

--- app/usuarios/middleware/inicio_obligatorio.py
+++ app/usuarios/middleware/inicio_obligatorio.py
@@ -7,10 +7,9 @@ class InicioObligatorio(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        return self.get_response(request)
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        if not 'iniciar_sesion' in request.META.get('PATH_INFO'):
+        ruta = request.META.get('PATH_INFO')
+        if not 'iniciar_sesion' in ruta and not ruta.startswith('/admin'):
             if not 'nombre' in request.session or request.session['nombre'] is None:
                 return redirect(reverse('usuarios:iniciar_sesion'))
-        return None
+
+        return self.get_response(request)
