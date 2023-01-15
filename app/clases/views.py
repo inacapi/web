ad46c75fa7d98a970c0c3e865195420b624be9c6
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-14 21:38:35 -0300

    Incluir el periodo en la url de las secciones

--- app/clases/views.py
+++ app/clases/views.py
@@ -16,15 +16,9 @@ def clase(request, id_clase):
     })
 
 
-def seccion(request, id_clase, id_seccion):
+def seccion(request, id_clase, id_periodo, id_seccion):
     clase = Clase.objects.get(id=id_clase)
     seccion = clase.secciones.get(id=id_seccion)
-    inscripciones = seccion.inscripciones.all()
-    inscripciones = map(lambda inscripcion: {
-        'notas': inscripcion.notas.all(),
-        'cantidad_notas': inscripcion.notas.count(),
-    }, inscripciones)
-    print(inscripciones)
     return render(request, 'clases/seccion.html', {
         'seccion': seccion
     })
