e43b4c63f1d26e01cb24986ad19937e6a3f12ce5
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-02-02 19:48:50 -0300

    Icono de la página y quitar volumen de datos en desarrollo

--- app/web/settings.py
+++ app/web/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'web', 'static')
+]
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
